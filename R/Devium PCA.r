61b75cf992c9e1c0d014b96b8907c482f0d9c191
dgrapov <dgrapov@gmail.com>
2013-11-24 16:31:07 -0800

    fixing pca for dveium-web

--- R/Devium PCA.r
+++ R/Devium PCA.r
@@ -10,8 +10,10 @@ devium.pca.calculate<-function(pca.inputs=get("devium.pca.object",envir=devium),
 		
 		#check for text or factor and remove (add to subset) 
 		tmp<-pca.inputs
-		data.obj<-as.data.frame(get(tmp$pca.data))
-		data.obj<-data.obj[sapply(1:ncol(data.obj), function(i) {class(data.obj[,i])=="numeric"|class(data.obj[,i])=="integer"})] # has to be better way to avoid factors
+		# data.obj<-as.data.frame(get(tmp$pca.data))
+		# data.obj<-data.obj[sapply(1:ncol(data.obj), function(i) {class(data.obj[,i])=="numeric"|class(data.obj[,i])=="integer"})] # has to be better way to avoid factors
+		 data.obj<-tmp$pca.data
+		
 		if(is.null(tmp$pca.cv)){tmp$pca.cv="none"} else {pca.cv<-tmp$pca.cv} #avoid issues elsewhere
 		
 		#adjust PCS if > than data
