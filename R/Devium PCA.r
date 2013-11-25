f90d52e9e662c722ea5ac75e5848b839f47ef0aa
dgrapov <dgrapov@gmail.com>
2013-11-25 09:13:22 -0800

    updating PCA plotting

--- R/Devium PCA.r
+++ R/Devium PCA.r
@@ -1,75 +1,6 @@
-devium.pca.calculate<-function(pca.inputs=get("devium.pca.object",envir=devium),args.list=TRUE,return=NULL, plot=TRUE)
-	{
-		#port of imDEV source code optimized for GUI use
-		#accepts list with the following arguments
-		#pca.data<- character name of the data object (samples as rows)
-		#pca.components numeric number of number of principal components
-		#pca.algorithm see pcaMethods::pca for options
-		#pca.center logical, mean center the data
-		#pca.scaling character describing scaling method, see pcaMethods::prep for options
-		
-		#check for text or factor and remove (add to subset) 
-		tmp<-pca.inputs
-		data.obj<-as.data.frame(get(tmp$pca.data))
-		data.obj<-data.obj[sapply(1:ncol(data.obj), function(i) {class(data.obj[,i])=="numeric"|class(data.obj[,i])=="integer"})] # has to be better way to avoid factors
-	
-		
-		if(is.null(tmp$pca.cv)){tmp$pca.cv="none"} else {pca.cv<-tmp$pca.cv} #avoid issues elsewhere
-		
-		#adjust PCS if > than data
-		PCs<-tmp$pca.components
-		if(PCs> min(dim(data.obj))){PCs<-min(dim(data.obj))} # this should be done internally in the PCa fxn
-		pca.results<-pcaMethods::pca(as.matrix(data.obj), method=tmp$pca.algorithm, 
-			nPcs=PCs, center=tmp$pca.center,scale=tmp$pca.scaling, cv = pca.cv, seed=123)
-		
-		#results
-		scores<-as.data.frame(pca.results@scores)
-		loadings<-as.data.frame(pca.results@loadings)
-		eigenvalues<-data.frame(eigenvalues=pca.results@R2)
-		
-		
-		if(tmp$pca.cv=="q2"){
-				# account for unequal r2 and q2 lengths 
-				q2<-tryCatch(Q2(pca.results), error=function(e) {0} )#some versions of pcaMEthods don't have this?
-				q2<-c(q2,rep(q2[length(q2)],nrow(eigenvalues)-length(q2)))
-				eigenvalues<-data.frame(eigenvalues,q2=q2)
-			}
 
-		#add leverage and dmodX
-		#bind between scores and loadings
-		lev<-tryCatch(as.matrix(leverage(pca.results)),error=function(e){"can not calculate"})
-		dmodx<-tryCatch(as.matrix(DModX(pca.results)),error=function(e){"can not calculate"})
-		diagnostics<-tryCatch(data.frame(leverage=lev,DmodX=dmodx),error=function(e){data.frame(Error="not applicable")})
-
-		#scree plot
-		if(plot==TRUE){ make.scree.plot(eigenvalues) }
-	
-		
-		#Determine placement of output for EXCEL
-		if(!return=="list"){
-			data.list<-list(eigenvalues,scores,diagnostics,loadings)
-			list.names<-matrix(c("eigenvalues","scores","diagnostics","loadings"))
-			start.row<-1;spacer<-1;start.col<-1
-			direction<-"horizontal"
-
-			#assign complete object to envir = devium
-			assign("devium.pca.results",list(eigenvalues=eigenvalues, scores=scores, loadings=loadings, diagnostics=diagnostics,
-			placement=list.placement.full(data.list,list.names,direction="horizontal",start.col,start.row,spacer)),envir=devium)
-		}
-		
-		#get the name of the data
-		if(return=="list"){
-				return(list(pca.scores = scores, pca.loadings =  loadings,pca.eigenvalues = eigenvalues, pca.diagnostics = diagnostics))
-			} else {
-				name<-tmp$pca.data
-				assign(paste(name,"pca.scores",sep="."),scores,envir=.GlobalEnv)
-				assign(paste(name,"pca.diagnostics",sep="."),diagnostics,envir=.GlobalEnv)
-				assign(paste(name,"pca.loadings",sep="."),loadings,envir=.GlobalEnv)
-		}
-	}
-
-#trying to debug issue in shiny 
-devium.pca.calculate2<-function(pca.inputs,args.list=TRUE,return=NULL, plot=TRUE)
+#trying to debug issue in shiny, pca.data is now an object instead of its name as a character
+devium.pca.calculate<-function(pca.inputs,args.list=TRUE,return=NULL, plot=TRUE)
 	{
 		#port of imDEV source code optimized for GUI use
 		#accepts list with the following arguments
@@ -85,7 +16,8 @@ devium.pca.calculate2<-function(pca.inputs,args.list=TRUE,return=NULL, plot=TRUE
 		# data.obj<-data.obj[sapply(1:ncol(data.obj), function(i) {class(data.obj[,i])=="numeric"|class(data.obj[,i])=="integer"})] # has to be better way to avoid factors
 		 data.obj<-tmp$pca.data
 		
-		if(is.null(tmp$pca.cv)){tmp$pca.cv="none"} else {pca.cv<-tmp$pca.cv} #avoid issues elsewhere
+		if(is.null(tmp$pca.cv)){pca.cv<-"none"} else {pca.cv<-tmp$pca.cv} #avoid issues elsewhere
+		
 		
 		#adjust PCS if > than data
 		PCs<-tmp$pca.components
@@ -99,7 +31,7 @@ devium.pca.calculate2<-function(pca.inputs,args.list=TRUE,return=NULL, plot=TRUE
 		eigenvalues<-data.frame(eigenvalues=pca.results@R2)
 		
 		
-		if(tmp$pca.cv=="q2"){
+		if(pca.cv=="q2"){
 				# account for unequal r2 and q2 lengths 
 				q2<-tryCatch(Q2(pca.results), error=function(e) {0} )#some versions of pcaMEthods don't have this?
 				q2<-c(q2,rep(q2[length(q2)],nrow(eigenvalues)-length(q2)))
@@ -191,16 +123,16 @@ make.scree.plot.bar<-function(eigenvalues){
 
 
 plot.PCA<-function(pca, results = c("screeplot","scores","loadings","biplot"),size=3,color=NULL, label=TRUE, legend.name =  NULL){
+	
+	library(ggplot2)
+	library(reshape2)
 	#results<-match.args(results) #
 	local<-switch(results[1],
 
 		"screeplot" 	= function(pca,...){make.scree.plot.bar(pca$pca.eigenvalues)},
-		"scores"		= function(pca,color, size){
+		"scores"		= function(pca,color,size){
 								obj<-pca$pca.scores[,]	
-								points<-if(is.null(color)) { geom_point(color="gray",size=size,alpha=.5,legend=FALSE) } else { geom_point(aes(color=color),size=size,alpha=.5)  } 	
-								labels<-if(label==TRUE){geom_text(size=size,aes(x=PC1, y=(PC2 -.5),color=color,label=id),legend = F)} else { NULL }
-								tmp<-data.frame(obj,id = rownames(obj),color=color)
-								
+								tmp<-data.frame(obj,id = rownames(obj))
 								#plot 
 								.theme2<- theme(
 											axis.line = element_line(colour = 'gray', size = .75), 
@@ -209,43 +141,92 @@ plot.PCA<-function(pca, results = c("screeplot","scores","loadings","biplot"),si
 											legend.background=element_rect(fill='white'),
 											legend.key = element_blank()
 										 )
+										 
+								if(is.null(color)){
+										tmp$color<-"gray"
+									}else{
+										tmp$color<-as.factor(color[,])
+										if(is.null(legend.name)){legend.name<-colnames(color)}
+								}
+								
+								points<-if(all(tmp$color=="gray")) { 
+									geom_point(color="gray",size=size,alpha=.75,show_guide = FALSE) 
+								} else { 
+									geom_point(aes(color=color),size=size,alpha=.5)  
+								}
+								#labels
+								lab.offset<-abs(range(obj[,2])[1]-range(obj[,2])[2])/20							
+								labels<-if(label==TRUE){geom_text(size=size,aes(x=PC1, y=(PC2 - lab.offset),label=id),color="black",show_guide = FALSE)} else { NULL }
+								
 								#Hoettellings T2 ellipse	 
 								ell<-get.ellipse.coords(cbind(obj[,1],obj[,2]), group=tmp$color)# group visualization via 
-								 
-								p<-ggplot(data=tmp,aes_string(x=colnames(tmp)[1], y=colnames(tmp)[2])) + geom_vline(xintercept = 0,linetype=2, size=.5, alpha=.5) + 
+								polygons<-if(is.null(color)){
+										geom_polygon(data=data.frame(ell$coords),aes(x=x,y=y), fill="gray", color="gray",linetype=2,alpha=.1, show_guide = FALSE) 
+									} else {
+										geom_polygon(data=data.frame(ell$coords),aes(x=x,y=y, fill=group),linetype=2,alpha=.1, show_guide = FALSE) 
+									}
+								
+								#making the actual plot 
+								p<-ggplot(data=tmp,aes_string(x=colnames(tmp)[1], y=colnames(tmp)[2])) + 
+								geom_vline(xintercept = 0,linetype=2, size=.5, alpha=.5) + 
 								geom_hline(yintercept = 0,linetype=2, size=.5, alpha=.5) +
 								points +
 								.theme2 + 
 								labels +
-								geom_polygon(data=data.frame(ell$coords),aes(x=x,y=y,fill=group, color=group),linetype=2,alpha=.1, legend=FALSE) +
+								polygons +
 								scale_x_continuous(sprintf("PC1 (%s%%)", round(pca$pca.eigenvalues[1,1],digits=2)*100))+
 								scale_y_continuous(sprintf("PC2 (%s%%)", round(pca$pca.eigenvalues[2,1],digits=2)*100)) 
 								if(!is.null(legend.name)) {p<-p+scale_colour_discrete(name = legend.name)}
 								print(p)
 							},
 							
-		"loadings"		= function(pca,size,...){
+		"loadings"		= function(pca,color,size){
 								obj<-pca$pca.loadings[,]	
-								points<-if(is.null(color)) { geom_point(color="gray",size=size,alpha=.5,legend=FALSE) } else { geom_point(aes(color=color),size=size,alpha=.5)  } 	
-								labels<-if(label==TRUE){geom_text(size=3,aes(x=PC1, y=(PC2 -.5),color=color,label=id),legend = F)} else {NULL}
-								tmp<-data.frame(obj,id = rownames(obj),color=color)
-								
+								tmp<-data.frame(obj,id = rownames(obj))
 								#plot 
 								.theme2<- theme(
 											axis.line = element_line(colour = 'gray', size = .75), 
 											panel.background = element_blank(), 
-											plot.background = element_blank()
+											plot.background = element_blank(),
+											legend.background=element_rect(fill='white'),
+											legend.key = element_blank()
 										 )
+										 
+								if(is.null(color)){
+										tmp$color<-"gray"
+									}else{
+										tmp$color<-as.factor(color[,])
+										if(is.null(legend.name)){legend.name<-colnames(color)}
+								}
 								
-								 
-								p<-ggplot(data=tmp,aes_string(x=colnames(tmp)[1], y=colnames(tmp)[2])) + geom_vline(xintercept = 0,linetype=2, size=.5, alpha=.5) + 
+								points<-if(all(tmp$color=="gray")) { 
+									geom_point(color="gray",size=size,alpha=.75,show_guide = FALSE) 
+								} else { 
+									geom_point(aes(color=color),size=size,alpha=.5)  
+								}
+								#labels
+								lab.offset<-abs(range(obj[,2])[1]-range(obj[,2])[2])/20							
+								labels<-if(label==TRUE){geom_text(size=size,aes(x=PC1, y=(PC2 - lab.offset),label=id),color="black",show_guide = FALSE)} else { NULL }
+								
+								#Hoettellings T2 ellipse	 
+								ell<-get.ellipse.coords(cbind(obj[,1],obj[,2]), group=tmp$color)# group visualization via 
+								polygons<-if(is.null(color)){
+										geom_polygon(data=data.frame(ell$coords),aes(x=x,y=y), fill="gray", color="gray",linetype=2,alpha=.1, show_guide = FALSE) 
+									} else {
+										geom_polygon(data=data.frame(ell$coords),aes(x=x,y=y, fill=group),linetype=2,alpha=.1, show_guide = FALSE) 
+									}
+								
+								#making the actual plot 
+								p<-ggplot(data=tmp,aes_string(x=colnames(tmp)[1], y=colnames(tmp)[2])) + 
+								geom_vline(xintercept = 0,linetype=2, size=.5, alpha=.5) + 
 								geom_hline(yintercept = 0,linetype=2, size=.5, alpha=.5) +
 								points +
 								.theme2 + 
-								labels+
-								geom_text(size=3,aes(x=PC1, y=(PC2 -.5),color=color,label=id),legend = F) +
+								labels +
+								polygons +
 								scale_x_continuous(sprintf("PC1 (%s%%)", round(pca$pca.eigenvalues[1,1],digits=2)*100))+
-								scale_y_continuous(sprintf("PC2 (%s%%)", round(pca$pca.eigenvalues[2,1],digits=2)*100))+
+								scale_y_continuous(sprintf("PC2 (%s%%)", round(pca$pca.eigenvalues[2,1],digits=2)*100)) 
+								if(!is.null(legend.name)) {p<-p+scale_colour_discrete(name = legend.name)}
 								print(p)
 							},
 							
@@ -282,12 +263,13 @@ plot.PCA<-function(pca, results = c("screeplot","scores","loadings","biplot"),si
 								tmp.loadings<-data.frame(tmp.loadings,label=rownames(loadings))
 								 p<-ggplot()+
 								 geom_point(data=scores, aes_string(x=colnames(data)[1], y=colnames(data)[2]))+
-								 geom_segment(data=tmp.loadings, aes_string(x=0, y=0, xend=colnames(data)[1], yend=colnames(data)[2]), arrow=arrow(length=unit(0.2,"cm")), alpha=0.25)+
+								 geom_segment(data=tmp.loadings, aes_string(x=0, y=0, xend=colnames(data)[1], yend=colnames(data)[2]), arrow=arrow(length=unit(0.05,"cm")), alpha=0.25)+
 								 geom_text(data=tmp.loadings, aes_string(x=colnames(data)[1], y=colnames(data)[2], label="label"), alpha=0.5, size=3)+
 								 scale_colour_discrete("Variety")+
 								 scale_x_continuous(sprintf("PC1 (%s%%)", round(pca$pca.eigenvalues[1,1],digits=2)*100))+
 								 scale_y_continuous(sprintf("PC2 (%s%%)", round(pca$pca.eigenvalues[2,1],digits=2)*100))+
 								 .theme2
+								 print(p)
 							}
 	)
 	
@@ -297,15 +279,22 @@ plot.PCA<-function(pca, results = c("screeplot","scores","loadings","biplot"),si
 
 	
 test<-function(){
-tmp<-list()
+data(mtcars)
+data<-mtcars
 
+tmp<-list()
 tmp$pca.algorithm<-"svd"
 tmp$pca.components<-2
 tmp$pca.center<-TRUE
 tmp$pca.scaling<-"uv"
-tmp$pca.data<-"data"
+tmp$pca.data<-data
 pca.inputs<-tmp
 
 res<-devium.pca.calculate(pca.inputs,return="list",plot=FALSE)
+
+results<-"biplot"#"scores","loadings","biplot")
+color<-NULL#data.frame(am=mtcars$am)
+plot.PCA(pca=res,results,size=3,color=color, label=TRUE, legend.name =  NULL)
+
 
 }
