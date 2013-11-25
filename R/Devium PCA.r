1d1bcd2d0e385bbe10736db6446d998e1b4f7165
dgrapov <dgrapov@gmail.com>
2013-11-24 16:32:32 -0800

    try resolve pca issues

--- R/Devium PCA.r
+++ R/Devium PCA.r
@@ -10,6 +10,77 @@ devium.pca.calculate<-function(pca.inputs=get("devium.pca.object",envir=devium),
 		
 		#check for text or factor and remove (add to subset) 
 		tmp<-pca.inputs
+		data.obj<-as.data.frame(get(tmp$pca.data))
+		data.obj<-data.obj[sapply(1:ncol(data.obj), function(i) {class(data.obj[,i])=="numeric"|class(data.obj[,i])=="integer"})] # has to be better way to avoid factors
+	
+		
+		if(is.null(tmp$pca.cv)){tmp$pca.cv="none"} else {pca.cv<-tmp$pca.cv} #avoid issues elsewhere
+		
+		#adjust PCS if > than data
+		PCs<-tmp$pca.components
+		if(PCs> min(dim(data.obj))){PCs<-min(dim(data.obj))} # this should be done internally in the PCa fxn
+		pca.results<-pcaMethods::pca(as.matrix(data.obj), method=tmp$pca.algorithm, 
+			nPcs=PCs, center=tmp$pca.center,scale=tmp$pca.scaling, cv = pca.cv, seed=123)
+		
+		#results
+		scores<-as.data.frame(pca.results@scores)
+		loadings<-as.data.frame(pca.results@loadings)
+		eigenvalues<-data.frame(eigenvalues=pca.results@R2)
+		
+		
+		if(tmp$pca.cv=="q2"){
+				# account for unequal r2 and q2 lengths 
+				q2<-tryCatch(Q2(pca.results), error=function(e) {0} )#some versions of pcaMEthods don't have this?
+				q2<-c(q2,rep(q2[length(q2)],nrow(eigenvalues)-length(q2)))
+				eigenvalues<-data.frame(eigenvalues,q2=q2)
+			}
+
+		#add leverage and dmodX
+		#bind between scores and loadings
+		lev<-tryCatch(as.matrix(leverage(pca.results)),error=function(e){"can not calculate"})
+		dmodx<-tryCatch(as.matrix(DModX(pca.results)),error=function(e){"can not calculate"})
+		diagnostics<-tryCatch(data.frame(leverage=lev,DmodX=dmodx),error=function(e){data.frame(Error="not applicable")})
+
+		#scree plot
+		if(plot==TRUE){ make.scree.plot(eigenvalues) }
+	
+		
+		#Determine placement of output for EXCEL
+		if(!return=="list"){
+			data.list<-list(eigenvalues,scores,diagnostics,loadings)
+			list.names<-matrix(c("eigenvalues","scores","diagnostics","loadings"))
+			start.row<-1;spacer<-1;start.col<-1
+			direction<-"horizontal"
+
+			#assign complete object to envir = devium
+			assign("devium.pca.results",list(eigenvalues=eigenvalues, scores=scores, loadings=loadings, diagnostics=diagnostics,
+			placement=list.placement.full(data.list,list.names,direction="horizontal",start.col,start.row,spacer)),envir=devium)
+		}
+		
+		#get the name of the data
+		if(return=="list"){
+				return(list(pca.scores = scores, pca.loadings =  loadings,pca.eigenvalues = eigenvalues, pca.diagnostics = diagnostics))
+			} else {
+				name<-tmp$pca.data
+				assign(paste(name,"pca.scores",sep="."),scores,envir=.GlobalEnv)
+				assign(paste(name,"pca.diagnostics",sep="."),diagnostics,envir=.GlobalEnv)
+				assign(paste(name,"pca.loadings",sep="."),loadings,envir=.GlobalEnv)
+		}
+	}
+
+	#trying to debug issue in shiny 
+devium.pca.calculate2<-function(pca.inputs,args.list=TRUE,return=NULL, plot=TRUE)
+	{
+		#port of imDEV source code optimized for GUI use
+		#accepts list with the following arguments
+		#pca.data<- character name of the data object (samples as rows)
+		#pca.components numeric number of number of principal components
+		#pca.algorithm see pcaMethods::pca for options
+		#pca.center logical, mean center the data
+		#pca.scaling character describing scaling method, see pcaMethods::prep for options
+		
+		#check for text or factor and remove (add to subset) 
+		tmp<-pca.inputs
 		# data.obj<-as.data.frame(get(tmp$pca.data))
 		# data.obj<-data.obj[sapply(1:ncol(data.obj), function(i) {class(data.obj[,i])=="numeric"|class(data.obj[,i])=="integer"})] # has to be better way to avoid factors
 		 data.obj<-tmp$pca.data
@@ -67,7 +138,8 @@ devium.pca.calculate<-function(pca.inputs=get("devium.pca.object",envir=devium),
 				assign(paste(name,"pca.loadings",sep="."),loadings,envir=.GlobalEnv)
 		}
 	}
-
+	
+	
 # generate a scree plot base
 make.scree.plot<-function(eigenvalues)
 	{
